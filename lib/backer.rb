8d2e467655da35f9350783efdf7582fdefbdc0ba
Christine Xiang <yutingcxiang@gmail.com>
2018-07-16 00:17:45 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,7 +1,8 @@
 class Backer
-  attr_accessor :backed_projects
+  attr_accessor :backed_projects, :name
   
-  def initialize
+  def initialize(name)
+    @name = name
     @backed_projects = []
   end
 end
