747426b52711b3e82c39297d04206784144b2503
Christine Xiang <yutingcxiang@gmail.com>
2018-07-16 00:20:16 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,13 @@
+class Backer
+  attr_accessor :backed_projects, :name
+  
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+  
+  def back_project(project)
+    @backed_projects << project
+    project.backers << self
+  end
+end
